d0530615283eab47c254fe75dac128eb9894c0b6
niall <Niallcoyr@gmail.com>
2023-04-18 23:12:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ca4sp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ca4sp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    //Views
+    Button mUserBtn,mAdministratorBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        mUserBtn=findViewById(R.id.userBtn);
+        mAdministratorBtn=findViewById(R.id.administratorBtn);
+
+        mUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UserLogin.class)); // User activity
+            }
+        });
+        mAdministratorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AdminLogin.class)); //Admin activity
+            }
+        });
+
+    }
+}
